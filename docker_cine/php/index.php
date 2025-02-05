2231f015717cfe12bb28f22d1b3c01d967efa9df
Alberto Villegas <alvich@floridauniversitaria.es>
2025-02-05 22:51:29 +0100

    Octavo commit

--- docker_cine/php/index.php
+++ docker_cine/php/index.php
@@ -22,7 +22,7 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listados de Películas</title>
+    <title>Listado de Películas</title>
 </head>
 
 <body>
